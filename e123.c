ef9346158ab93003e4f6d8570cb04d1e7c2d3ce9
Lys <elyesseen@gmail.com>
2024-09-07 07:31:22 +0100

    d

--- e123.c
+++ e123.c
@@ -17,19 +17,19 @@
 #include <stdio.h>
 
 int main() {
-    int i, n = 10;
-    printf("Numbers from 1 to %d:\n", n);
-    for (i = 1; i <= n; i++) 
-        printf("%d ", i);
-    
-    int sum = 0;
-    printf("Sum of numbers from 1 to %d is %d\n", n, sum);
-    printf("^^ don't remove me");
-    printf("^& nor me &^");
-    printf("^& nor me ");
-    printf(" nor me &^");
+	int i, n = 10;
+	printf("Numbers from 1 to %d:\n", n);
+	for (i = 1; i <= n; i++) 
+		printf("%d ", i);
 
-    return 0;
+	int sum = 0;
+	printf("Sum of numbers from 1 to %d is %d\n", n, sum);
+	printf("^^ don't remove me");
+	printf("^& nor me &^");
+	printf("^& nor me ");
+	printf(" nor me &^");
+
+	return 0;
 }
 
 */
@@ -38,17 +38,34 @@ int main() {
 void write_buff(char *buff)
 {
 	int i,c;
-	int in_comment = false;
-	bool in_qoute = false;
 	i = 0;
 
 	while ((c = getchar()) != EOF)
 	{
+		if (c == '"' )
+		{
+			buff[i++] = c;
+			while ((c = getchar()) != EOF && c != '"')
+				buff[i++] = c;
+		}
 		if (c == '^')
 		{
-			if ((c = getchar()))
+			if ((c = getchar()) == '^')
+				while ((c = getchar()) != '\n' && c != EOF)
+					continue ;
+			else if (c == '&')
+				{
+					while ((c = getchar()) != '&' && c != EOF)
+					{
+						if ((c = getchar()) == '^' || c == EOF )
+							break;
+						else
+							continue ;
+					}
+				}
+			else
+				buff[i++] = '^';
 		}
-
 		buff[i++] = c;
 	}
 	buff[i] = '\0';
